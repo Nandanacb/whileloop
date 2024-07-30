69dc63e560189e711d55d97b06528f19caa64068
Nandanacb <nandhnacb@gmail.com>
2024-07-30 13:34:08 +0530

    whileloop 30/7/2024

---
+++ program1.dart
@@ -0,0 +1,14 @@
+void main()
+{
+
+int sum=0;
+int i=1;
+
+while(i<=100)
+{
+sum=sum+i;
+i++;
+}
+print("sum=$sum");
+
+}
---
+++ program2.dart
@@ -0,0 +1,12 @@
+void main(){
+
+int i=1;
+
+while(i<=10){
+print("square of $i is ${i*i} ");
+i++;
+}
+}
+
+
+
